926b792029fef9a42ddcf33ee6bd0f6a14dbbbd9
pitzee <pitzee421@gmail.com>
2023-07-07 10:25:20 -0700

    do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,21 +7,44 @@ from fabric.api import *
 from datetime import datetime
 import os
 
+
 env.hosts = ['3.83.238.226', '34.202.234.56']
 env.user = 'ubuntu'
 
 
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder
+    """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        """ return none if not succed """
+        return None
+
+
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    
-    if not exists(archive_path):
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
         return False
-    archive_file = archive_path.split("/")[-1]
-    archive_folder = archive_file.split(".")[0]
-    put(archive_path, "/tmp/")
-    run("sudo mkdir -p /data/web_static/releases/{}".format(archive_folder))
-    run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(archive_file, archive_folder))
-    run("sudo rm /tmp/{}".format(archive_file))
-    run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(archive_folder))
-    return True
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
+    try:
+        put(archive_path, '/tmp/')
+        run('sudo mkdir -p {}'.format(releases_path))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('sudo mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('sudo rm -rf {}/web_static'.format(releases_path))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
+        return True
+    except:
+        return False
