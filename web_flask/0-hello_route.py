cd2edbad773ccf385e56194c153c47fa63fcaac4
pitzee <pitzee421@gmail.com>
2023-07-22 08:10:38 -0700

    web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-#Script to start a Flask web application
+""" 0. Script to start a Flask web application """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    #return a string when the route is accessed
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
