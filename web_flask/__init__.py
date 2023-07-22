8df12bde4a0d8ef48a351c1e535fc89c443f83be
pitzee <pitzee421@gmail.com>
2023-07-22 07:58:21 -0700

    web_flask

--- web_flask/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .0-hello_route import hello_hbnb
-
-app = Flask(__name__)
-
-app.register_blueprint(hello_hbnb)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
